f55c5ce5f3aa7211abf26833f10e7e35afad166f
thecrazydriver1 <impreza22b@proton.me>
2024-03-07 12:09:08 +0100

    szamtani kepletek

--- js/szamtani.js
+++ js/szamtani.js
@@ -1,7 +1,20 @@
-function an(a1, d, n) {
+function szt_an(a1, d, n) {
     return a1+(n-1)*d;
 }
 
-function Sn(a1, d, n) {
-    return (n/2)*(2*a1+)
+function szt_Sn(a1, d, n) {
+    return (n/2)*(2*a1+(n-1)*d);
+}
+
+function szt_atlag(a1, d, n) {
+    return szt_Sn(a1, d, n)/n;
+}
+
+function szt_szoras(a1, d, n) {
+    let szoras = 0;
+    const atlag = szt_atlag(a1, d, n);
+    for (let i=0; i<n; i++) {
+        szoras += (szt_an(a1, d, i)-atlag)**2;
+    }
+    return Math.sqrt(szoras/n);
 }
